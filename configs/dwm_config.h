92540e183394f9b76429cbbfc4f5f3b195e4bafa
Marco Lucidi <markolucidi@gmail.com>
2019-11-16 17:52:06 +0100

    removed Gimp from rules array
    inserted a placeholder since array can't be empty

--- configs/dwm_config.h
+++ configs/dwm_config.h
@@ -26,8 +26,8 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
+	/* class         instance    title       tags mask     isfloating   monitor */
+	{ "PlaceHolder", NULL,       NULL,       0,            1,           -1 },
 };
 
 /* layout(s) */
